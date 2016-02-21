2e35bd4abc1bb2ed6fc503db0f432b58e6b53689
Jose Lorenzo Yu <enzoyu5488@Joses-MacBook-Pro-2.local>
2016-02-21 17:10:51 +0800

    First Application instance

--- TrialFirstApplication/ViewController.swift
+++ TrialFirstApplication/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var RegisterButton: UIButton!
+    @IBOutlet weak var UserNameText: UITextField!
+    @IBOutlet weak var PasswordText: UITextField!
+    @IBOutlet weak var NameText: UITextField!
+    @IBOutlet weak var AgeText: UITextField!
+    @IBOutlet weak var Logo: UIImageView!
+    @IBOutlet weak var UNStatic: UILabel!
+    @IBOutlet weak var PWStatic: UILabel!
+    @IBOutlet weak var NameStatic: UILabel!
+    @IBOutlet weak var AgeStatic: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +32,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func OpenLogin(sender: AnyObject) {
+        UserNameText.hidden = false;
+        PasswordText.hidden = false;
+        NameText.hidden = true;
+        AgeText.hidden = true;
+        UNStatic.hidden = false;
+        PWStatic.hidden = false;
+        NameStatic.hidden = true;
+        AgeStatic.hidden = true;
+    }
+    @IBAction func OpenRegister(sender: AnyObject) {
+        UserNameText.hidden = false;
+        PasswordText.hidden = false;
+        NameText.hidden = false;
+        AgeText.hidden = false;
+        UNStatic.hidden = false;
+        PWStatic.hidden = false;
+        NameStatic.hidden = false;
+        AgeStatic.hidden = false;
+    }
 
 }
 
